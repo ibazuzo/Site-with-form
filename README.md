c17cad0417ff11bbdca8251e18998d0bf5145a21
Unknown <icvasilescu@gmail.com>
2019-01-31 00:46:28 +0200

    Add individual form field validation, some HTML/CSS fixes, update Readme.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Site-with-form
 
-Small site with form using Bootstrap.
+Small site with form using HTML/CSS, Bootstrap and JS
+
+* Custom form validation done through JavaScript. Browser default validation is disabled.
+* Validation errors are displayed right after losing focus from the form field.
+* Submit status is confirmed through Succes/Invalid message.
+
+![Site with form](https://github.com/ibazuzo/Site-with-form/blob/master/screenshot.jpg)
--- main.js
+++ main.js
@@ -4,12 +4,204 @@ function init() {
     const form = document.getElementById('form');
     form.onsubmit = submit;
 
-    
-    
+
+    document.addEventListener('blur', function (event) {
+
+        formValidation[event.target.id] = event.target.validity.valid;
+
+        var error = hasError(event.target);
+
+        if (error) {
+            showError(event.target, error);
+            return;
+        }
+
+        removeError(event.target);
+
+    }, true);
+
+    document.addEventListener('submit', function (event) {
+
+        // Get all of the form elements
+        var fields = event.target.elements;
+
+        // Validate each field
+        // Store the first field with an error to a variable so we can bring it into focus later
+        var error, hasErrors;
+        for (var i = 0; i < fields.length; i++) {
+            error = hasError(fields[i]);
+            if (error) {
+                showError(fields[i], error);
+                if (!hasErrors) {
+                    hasErrors = fields[i];
+                }
+            }
+        }
+
+        // If there are errrors, don't submit form and focus on first element with error
+        if (hasErrors) {
+            event.preventDefault();
+            hasErrors.focus();
+        }
+
+        // Otherwise, let the form submit normally
+        // You could also bolt in an Ajax form submit process here
+
+    }, false);
 }
 
+// Validate the field
+function hasError(field) {
+
+    // Don't validate submits, buttons, file and reset inputs, and disabled fields
+    if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
+
+    // Get validity
+    var validity = field.validity;
+
+    // If valid, return null
+    if (validity.valid) return;
+
+    // If field is required and empty
+    if (validity.valueMissing) return 'Please fill out this field.';
+
+    // If not the right type
+    if (validity.typeMismatch) {
+
+        // Email
+        if (field.type === 'email') return 'Please enter an email address.';
 
 
+    }
+
+    // If too short
+    if (validity.tooShort) return 'Please lengthen this text to ' + field.getAttribute('minLength') + ' characters or more. You are currently using ' + field.value.length + ' characters.';
+
+    // If too long
+    if (validity.tooLong) return 'Please shorten this text to no more than ' + field.getAttribute('maxLength') + ' characters. You are currently using ' + field.value.length + ' characters.';
+
+    // If number input isn't a number
+    if (validity.badInput) return 'Please enter a number.';
+
+    // If a number value doesn't match the step interval
+    if (validity.stepMismatch) return 'Please select a valid value.';
+
+    // If a number field is over the max
+    if (validity.rangeOverflow) return 'Please select a value that is no more than ' + field.getAttribute('max') + '.';
+
+    // If a number field is below the min
+    if (validity.rangeUnderflow) return 'Please select a value that is no less than ' + field.getAttribute('min') + '.';
+
+    // If pattern doesn't match
+    if (validity.patternMismatch) {
+
+        // If pattern info is included, return custom error
+        if (field.hasAttribute('title')) return field.getAttribute('title');
+
+        // Otherwise, generic error
+        return 'Please match the requested format.';
+
+    }
+
+    // If all else fails, return a generic catchall error
+    return 'The value you entered for this field is invalid.';
+
+};
+
+function showError(field, error) {
+
+    // Add error class to field
+    field.classList.add('error');
+
+    // If the field is a radio button and part of a group, error all and get the last item in the group
+    if (field.type === 'radio' && field.name) {
+        var group = document.getElementsByName(field.name);
+        if (group.length > 0) {
+            for (var i = 0; i < group.length; i++) {
+                // Only check fields in current form
+                if (group[i].form !== field.form) continue;
+                group[i].classList.add('error');
+            }
+            field = group[group.length - 1];
+        }
+    }
+
+    // Get field id or name
+    var id = field.id || field.name;
+    if (!id) return;
+
+    // Check if error message field already exists
+    // If not, create one
+    var message = field.form.querySelector('.error-message#error-for-' + id);
+    if (!message) {
+        message = document.createElement('div');
+        message.className = 'error-message';
+        message.id = 'error-for-' + id;
+
+        // If the field is a radio button or checkbox, insert error after the label
+        var label;
+        if (field.type === 'radio' || field.type === 'checkbox') {
+            label = field.form.querySelector('label[for="' + id + '"]') || field.parentNode;
+            if (label) {
+                label.parentNode.insertBefore(message, label.nextSibling);
+            }
+        }
+
+        // Otherwise, insert it after the field
+        if (!label) {
+            field.parentNode.insertBefore(message, field.nextSibling);
+        }
+    }
+
+    // Add aria role to the field for screen readers
+    field.setAttribute('aria-describedby', 'error-for-' + id);
+
+    // Update error message
+    message.innerHTML = error;
+
+    // Show error message
+    message.style.display = 'block';
+    message.style.visibility = 'visible';
+
+}
+
+// Remove error message
+function removeError(field) {
+
+    // Remove error class to field
+    field.classList.remove('error');
+
+    // If the field is a radio button and part of a group, remove error from all and get the last item in the group
+    if (field.type === 'radio' && field.name) {
+        var group = document.getElementsByName(field.name);
+        if (group.length > 0) {
+            for (var i = 0; i < group.length; i++) {
+                // Only check fields in current form
+                if (group[i].form !== field.form) continue;
+                group[i].classList.remove('error');
+            }
+            field = group[group.length - 1];
+        }
+    }
+
+    // Remove aria role from the field
+    field.removeAttribute('aria-describedby');
+
+    // Get field id or name
+    let id = field.id || field.name;
+    if (!id) return;
+
+    // Check if an error message is in the DOM
+    let message = field.form.querySelector('.error-message#error-for-' + id + '');
+    if (!message) return;
+
+    // If error message is found, hide it
+    message.innerHTML = '';
+    message.style.display = 'none';
+    message.style.visibility = 'hidden';
+}
+
+// Check the validity of the whole form
 function isFormValid() {
     for (key in formValidation) {
         if (formValidation[key] === false) {
@@ -22,12 +214,13 @@ function isFormValid() {
 
 function submit() {
     event.preventDefault();
-    
+
+    // If the form has at least one invalid field, submit is aborted
     if (!isFormValid()) {
-        console.log('asd');
         return;
     }
 
+    // Reset the validation html for the message that printed on screen regarding the submision status
     resetServerResponse();
 
     const url = 'https://actum-form-ulcrunoxba.now.sh/api/submit';
@@ -43,6 +236,7 @@ function submit() {
 
     const data = { name, surname, date, email, gender, childrens };
 
+    // Get the response from server
     fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -55,14 +249,18 @@ function submit() {
 function showResponseMessage(response) {
     const messageContainer = document.getElementById('serverResponse');
 
+    // Add the coresponding class for the submition status message
     if (response.success) {
         messageContainer.classList.add("alert-success");
     } else {
         messageContainer.classList.add('alert-danger');
     }
+
+    // Set the submition status message
     messageContainer.innerHTML = response.message;
 }
 
+// Reset the validation html for the message that printed on screen regarding the submision status
 function resetServerResponse() {
     const messageContainer = document.getElementById('serverResponse');
 
